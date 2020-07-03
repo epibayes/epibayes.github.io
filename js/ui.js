b5196c88b996e9b4d3221838f809e30933ec61e5
Alex <caoa@umich.edu>
2020-07-02 21:58:42 -0400

    code clean up; switch to d3 syntax

--- js/ui.js
+++ js/ui.js
@@ -56,12 +56,17 @@ function initToggles() {
     generateEmbedURL()
 }
 
+function generateEmbedURL() {
+    const query_string = `status=${status}&metric=${metric}`
+    const embeddableLink = `<iframe width="550px" height="500px" src="https://covidmapping.org/embedmap.html?${query_string}></iframe>`
+    d3.select('#embeddable').text(embeddableLink)
+}
+
 function updateDateRange(metric) {
     const endDate = getDateFromSlider()
     const key = metric.replace('rate','')
     startDate = key === 'cumulative' ? minDate : d3.max([minDate, d3.timeDay.offset(endDate, -(N-1))])
     setDateRange(startDate, endDate)
-    generateEmbedURL()
 }
 
 function setDateRange(startDate, endDate) {
@@ -97,5 +102,27 @@ function updateHexGrid() {
 function updateTotalInfo() {
     updateTotal(metric)
     updateDateRange(metric)    
-    generateEmbedURL()
+}
+
+function toggleEmbed() {
+    let iframeDiv = d3.select('#embeddable');
+    if (iframeDiv.style('display') === 'block'){
+        iframeDiv.style('display','none');
+        d3.select('#embedToggle').html('<i class="fa fa-code" aria-hidden="true"></i> Embed Map')
+        d3.select('#copyButton').style('display','none')
+    } else {
+        iframeDiv.style('display','block')
+        d3.select('#copyButton').style('display','inline-block')
+        d3.select('#embedToggle').html('Hide Window')
+    }
+}
+
+function copyEmbed() {
+    let theCode = d3.select('#embeddable').text();
+    let textArea = document.createElement('textarea');
+    textArea.textContent = theCode;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    alert('Copied.');
 }
