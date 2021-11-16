17679e2743e8d1c190b500848c255c04b6f9d8cf
choisteph <stchoi@umich.edu>
2021-11-16 11:42:17 -0500

    update scale and legend tick creation behavior

--- js/init.js
+++ js/init.js
@@ -50,7 +50,7 @@ let metric = 'cumulative';
 let colorScales = {
     'cases' : {
         'cumulative': d3.scaleSequentialLog(d3.interpolateYlOrRd).domain([1, 100000]).clamp(true),
-        'cumulativerate': d3.scaleSequential(d3.interpolateYlOrRd).domain([0, 12000]).clamp(true),
+        'cumulativerate': d3.scaleSequential(d3.interpolateYlOrRd).domain([0, 18000]).clamp(true),
         'weekly': d3.scaleSequentialLog(d3.interpolateYlGnBu).domain([1, 1000]).clamp(true),
         'weeklyrate': d3.scaleSequential(d3.interpolateYlGnBu).domain([0, 800]).clamp(true),
     },
